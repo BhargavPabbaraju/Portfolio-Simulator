430076c0d16d955aa55f10da83edfcc65b899677
sreekaranne1 <sreekaranne@Sreekars-MacBook-Pro.local>
2022-10-31 14:19:32 -0400

    added tests

--- test/ControllerImplTest.java
+++ test/ControllerImplTest.java
@@ -15,9 +15,8 @@ import static org.junit.Assert.assertEquals;
 public class ControllerImplTest {
 
 
-
   @Test
-  public void testCreateExistingUser(){
+  public void testCreateExistingUser() {
     Model model = new ModelImpl();
 
     ByteArrayInputStream in;
@@ -29,7 +28,7 @@ public class ControllerImplTest {
 
     View view = new ViewImpl(out);
 
-    ControllerImpl controller = new ControllerImpl(model,view,in);
+    ControllerImpl controller = new ControllerImpl(model, view, in);
     controller.go();
 
     String expectedOutput = "To select a particular option,enter the number next to it\n" +
@@ -49,11 +48,11 @@ public class ControllerImplTest {
             "4.Get Total Value on certain date\n" +
             "5.Save\n" +
             "6.Exit\n";
-    assertEquals(expectedOutput,bytes.toString());
+    assertEquals(expectedOutput, bytes.toString());
   }
 
   @Test
-  public void testCreateUserInvalidName(){
+  public void testCreateUserInvalidName() {
     Model model = new ModelImpl();
 
     ByteArrayInputStream in;
@@ -65,7 +64,7 @@ public class ControllerImplTest {
 
     View view = new ViewImpl(out);
 
-    ControllerImpl controller = new ControllerImpl(model,view,in);
+    ControllerImpl controller = new ControllerImpl(model, view, in);
     controller.go();
 
     String expectedOutput = "To select a particular option,enter the number next to it\n" +
@@ -85,7 +84,7 @@ public class ControllerImplTest {
             "4.Get Total Value on certain date\n" +
             "5.Save\n" +
             "6.Exit\n";
-    assertEquals(expectedOutput,bytes.toString());
+    assertEquals(expectedOutput, bytes.toString());
   }
 
   @Test
@@ -261,5 +260,123 @@ public class ControllerImplTest {
     assertEquals(expectedOutput, bytes.toString());
   }
 
+  @Test
+  public void testCreatePortfolioInvalidName() {
+    Model model = new ModelImpl();
 
+    ByteArrayInputStream in;
+
+    String input = "1 user3 1900 1 retire:{54 6";
+    in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bytes);
+
+    View view = new ViewImpl(out);
+
+    ControllerImpl controller = new ControllerImpl(model, view, in);
+    controller.go();
+    String expectedOutput = "To select a particular option,enter the number next to it\n" +
+            "1.Create User\n" +
+            "2.Load User\n" +
+            "Enter username\n" +
+            "Enter initial balance\n" +
+            "User successfully created\n" +
+            "1.Create Portfolio\n" +
+            "2.Load Portfolio\n" +
+            "3.Get Composition\n" +
+            "4.Get Total Value on certain date\n" +
+            "5.Save\n" +
+            "6.Exit\n" +
+            "Enter portfolio name\n" +
+            "Portfolio name must not contain any of \\\"{}[],: characters.\n" +
+            "1.Create Portfolio\n" +
+            "2.Load Portfolio\n" +
+            "3.Get Composition\n" +
+            "4.Get Total Value on certain date\n" +
+            "5.Save\n" +
+            "6.Exit\n";
+    assertEquals(expectedOutput, bytes.toString());
+
+  }
+
+  @Test
+  public void testCreatePortfolioExistingName() {
+    Model model = new ModelImpl();
+
+    ByteArrayInputStream in;
+
+    String input = "2 user1 1 retirement 6";
+    in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bytes);
+
+    View view = new ViewImpl(out);
+
+    ControllerImpl controller = new ControllerImpl(model, view, in);
+    controller.go();
+    String expectedOutput = "To select a particular option,enter the number next to it\n" +
+            "1.Create User\n" +
+            "2.Load User\n" +
+            "Enter username\n" +
+            "User successfully loaded\n" +
+            "1.Create Portfolio\n" +
+            "2.Load Portfolio\n" +
+            "3.Get Composition\n" +
+            "4.Get Total Value on certain date\n" +
+            "5.Save\n" +
+            "6.Exit\n" +
+            "Enter portfolio name\n" +
+            "Portfolio already exists\n" +
+            "1.Create Portfolio\n" +
+            "2.Load Portfolio\n" +
+            "3.Get Composition\n" +
+            "4.Get Total Value on certain date\n" +
+            "5.Save\n" +
+            "6.Exit\n";
+    assertEquals(expectedOutput, bytes.toString());
+  }
+
+  @Test
+  public void testCreatePortfolio() {
+    Model model = new ModelImpl();
+
+    ByteArrayInputStream in;
+
+    String input = "2 user1 1 personal AAPL 10 1 ORCL 20 2 6";
+    in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bytes);
+
+    View view = new ViewImpl(out);
+
+    ControllerImpl controller = new ControllerImpl(model, view, in);
+    controller.go();
+    String expectedOutput = "To select a particular option,enter the number next to it\n" +
+            "1.Create User\n" +
+            "2.Load User\n" +
+            "Enter username\n" +
+            "User successfully loaded\n" +
+            "1.Create Portfolio\n" +
+            "2.Load Portfolio\n" +
+            "3.Get Composition\n" +
+            "4.Get Total Value on certain date\n" +
+            "5.Save\n" +
+            "6.Exit\n" +
+            "Enter portfolio name\n" +
+            "Enter Stock Symbol\n" +
+            "Enter number of shares\n" +
+            "1.Add new stock\n" +
+            "2.Back to main menu\n" +
+            "Enter Stock Symbol\n" +
+            "Enter number of shares\n" +
+            "1.Add new stock\n" +
+            "2.Back to main menu\n" +
+            "1.Create Portfolio\n" +
+            "2.Load Portfolio\n" +
+            "3.Get Composition\n" +
+            "4.Get Total Value on certain date\n" +
+            "5.Save\n" +
+            "6.Exit\n";
+    assertEquals(expectedOutput, bytes.toString());
+  }
 }
