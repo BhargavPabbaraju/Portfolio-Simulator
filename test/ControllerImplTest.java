3185c959e6eced5511774b7d5cb8e60a1d1fa5ba
BHARGAV P <bhargav@BHARGAVs-MacBook-Air.local>
2022-10-31 12:58:32 -0400

    added create user tests

--- test/ControllerImplTest.java
+++ test/ControllerImplTest.java
@@ -51,4 +51,42 @@ public class ControllerImplTest {
             "6.Exit\n";
     assertEquals(expectedOutput,bytes.toString());
   }
+
+  @Test
+  public void testCreateUserInvalidName(){
+    Model model = new ModelImpl();
+
+    ByteArrayInputStream in;
+
+    String input = "1 user{][:,1 1900 2 user1 6";
+    in = new ByteArrayInputStream(input.getBytes());
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bytes);
+
+    View view = new ViewImpl(out);
+
+    ControllerImpl controller = new ControllerImpl(model,view,in);
+    controller.go();
+
+    String expectedOutput = "To select a particular option,enter the number next to it\n" +
+            "1.Create User\n" +
+            "2.Load User\n" +
+            "Enter username\n" +
+            "Enter initial balance\n" +
+            "Username must not contain any of \\\"{}[],: characters.\n" +
+            "To select a particular option,enter the number next to it\n" +
+            "1.Create User\n" +
+            "2.Load User\n" +
+            "Enter username\n" +
+            "User successfully loaded\n" +
+            "1.Create Portfolio\n" +
+            "2.Load Portfolio\n" +
+            "3.Get Composition\n" +
+            "4.Get Total Value on certain date\n" +
+            "5.Save\n" +
+            "6.Exit\n";
+    assertEquals(expectedOutput,bytes.toString());
+  }
+
+
 }
