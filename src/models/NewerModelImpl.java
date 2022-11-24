87bd6f7b32776756543512988450973f10d48794
sreekaranne1 <sreekaranne@Sreekars-MacBook-Pro.local>
2022-11-23 21:47:34 -0500

    dollar costing and invest

---
+++ src/models/NewerModelImpl.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.HashMap;
+
+public class NewerModelImpl extends NewModelImpl implements NewerModel {
+  @Override
+  public void investIntoPortfolio(String date, float amount, float transactionCost, HashMap<String, Float> stocks, ApiType apiType) {
+    this.user.investIntoPortfolio(date, amount, transactionCost, stocks, apiType);
+
+  }
+
+  @Override
+  public void createDollarCostStrategyPortfolio(String startDate, String endDate, int interval, float amount, float transactionCost, HashMap<String, Float> stocks) {
+    this.user.createDollarCostStrategyPortfolio(startDate, endDate, interval, amount, transactionCost, stocks);
+  }
+}
---
+++ test/models/NewerModelImplTest.java
@@ -0,0 +1,88 @@
+package models;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class NewerModelImplTest {
+  @Test
+  public void testInsertIntoPortfolio() throws IOException, ParseException {
+    NewerModel m = new NewerModelImpl();
+    m.loadUser("user5");
+    m.createPortfolio("flex", true);
+    HashMap<String, Float> stocks = new HashMap<>();
+    stocks.put("META", 40F);
+    stocks.put("AAPL", 40F);
+    stocks.put("MSFT", 20F);
+    m.investIntoPortfolio("2022-11-21", 2000, 10, stocks, ApiType.ALPHA_VANTAGE);
+    System.out.println(m.getComposition("2022-11-21"));
+    System.out.println(m.getCostBasis("2022-11-21", ApiType.ALPHA_VANTAGE));
+
+  }
+
+  @Test
+  public void testDollarCostBasisWithEndDate() throws IOException, ParseException {
+    NewerModel m = new NewerModelImpl();
+    m.loadUser("user5");
+    m.createPortfolio("flex", true);
+    HashMap<String, Float> stocks = new HashMap<>();
+    stocks.put("META", 40F);
+    stocks.put("AAPL", 40F);
+    stocks.put("MSFT", 20F);
+    m.createDollarCostStrategyPortfolio("2022-11-21", "2023-04-23", 30, 1000, 10, stocks);
+    System.out.println(m.getCostBasis("2022-11-20", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-11-21", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-11-30", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-12-21", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-12-22", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2023-03-30", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2023-04-23", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2023-04-25", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2026-04-25", ApiType.ALPHA_VANTAGE));
+  }
+
+  @Test
+  public void testDollarCostBasisWithOutEndDate() throws IOException, ParseException {
+    NewerModel m = new NewerModelImpl();
+    m.loadUser("user5");
+    m.createPortfolio("flex", true);
+    HashMap<String, Float> stocks = new HashMap<>();
+    stocks.put("META", 40F);
+    stocks.put("AAPL", 40F);
+    stocks.put("MSFT", 20F);
+    m.createDollarCostStrategyPortfolio("2022-11-21", null, 30, 1000, 10, stocks);
+    System.out.println(m.getCostBasis("2022-11-20", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-11-21", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-11-30", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-12-21", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2022-12-22", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2023-03-30", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2023-04-23", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2023-04-25", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getCostBasis("2026-04-25", ApiType.ALPHA_VANTAGE));
+  }
+
+  @Test
+  public void testDollarGetValue() throws IOException, ParseException {
+    NewerModel m = new NewerModelImpl();
+    m.loadUser("user5");
+    m.createPortfolio("flex", true);
+    HashMap<String, Float> stocks = new HashMap<>();
+    stocks.put("META", 40F);
+    stocks.put("AAPL", 40F);
+    stocks.put("MSFT", 20F);
+    m.createDollarCostStrategyPortfolio("2022-09-20", "2023-11-21", 30, 1000, 10, stocks);
+    System.out.println(m.getTotalValue("2022-09-19", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getTotalValue("2022-09-21", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getTotalValue("2022-09-30", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getTotalValue("2022-10-21", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getTotalValue("2022-10-24", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getTotalValue("2022-10-31", ApiType.ALPHA_VANTAGE));
+    System.out.println(m.getTotalValue("2022-11-21", ApiType.ALPHA_VANTAGE));
+  }
+
+}
